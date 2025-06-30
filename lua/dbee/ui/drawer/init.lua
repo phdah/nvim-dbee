873f29797b68f0d4c02f4a38ace3b6f9500fca39
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-06-30 18:22:47 +0200

    Add save for schema struct

--- lua/dbee/ui/drawer/init.lua
+++ lua/dbee/ui/drawer/init.lua
@@ -16,7 +16,7 @@ local expansion = require("dbee.ui.drawer.expansion")
 ---@field action_1? drawer_node_action primary action if function takes a second selection parameter, pick_items get picked before the call
 ---@field action_2? drawer_node_action secondary action if function takes a second selection parameter, pick_items get picked before the call
 ---@field action_3? drawer_node_action tertiary action if function takes a second selection parameter, pick_items get picked before the call
----@field lazy_children? fun():DrawerUINode[] lazy loaded child nodes
+---@field lazy_children? fun(refresh):DrawerUINode[] lazy loaded child nodes
 
 ---@class DrawerUI
 ---@field private tree NuiTree
@@ -116,7 +116,7 @@ function DrawerUI:on_current_connection_changed(data)
     return
   end
   self.current_conn_id = data.conn_id
-  self:refresh()
+  self:refresh(false)
 end
 
 -- event listener for current note change
@@ -127,7 +127,7 @@ function DrawerUI:on_current_note_changed(data)
     return
   end
   self.current_note_id = data.note_id
-  self:refresh()
+  self:refresh(false)
 end
 
 ---@private
@@ -205,7 +205,7 @@ function DrawerUI:get_actions()
 
     -- if function for getting layout exist, call it
     if not expanded and type(node.lazy_children) == "function" then
-      self.tree:set_nodes(node.lazy_children(), node.id)
+      self.tree:set_nodes(node.lazy_children(false), node.id)
     end
 
     node:expand()
@@ -221,7 +221,7 @@ function DrawerUI:get_actions()
     end
 
     action(function()
-      self:refresh()
+      self:refresh(false)
     end, function(opts)
       opts = opts or {}
       menu.select {
@@ -245,7 +245,7 @@ function DrawerUI:get_actions()
 
   return {
     refresh = function()
-      self:refresh()
+      self:refresh(true)
     end,
     action_1 = function()
       local node = self.tree:get_node() --[[@as DrawerUINode]]
@@ -307,12 +307,12 @@ function DrawerUI:do_action(action)
 end
 
 ---Refreshes the tree.
-function DrawerUI:refresh()
+function DrawerUI:refresh(load)
   -- assemble tree layout
   ---@type DrawerUINode[]
   local nodes = {}
   local editor_nodes = convert.editor_nodes(self.editor, self.current_conn_id, function()
-    self:refresh()
+    self:refresh(false)
   end)
   for _, ly in ipairs(editor_nodes) do
     table.insert(nodes, ly)
@@ -329,7 +329,7 @@ function DrawerUI:refresh()
 
   local exp = expansion.get(self.tree)
   self.tree:set_nodes(nodes)
-  expansion.set(self.tree, exp)
+  expansion.set(self.tree, exp, load)
 
   self.tree:render()
 end
@@ -344,7 +344,7 @@ function DrawerUI:show(winid)
   -- configure window options (needs to be set after setting the buffer to window)
   common.configure_window_options(self.winid, self.window_options)
 
-  self:refresh()
+  self:refresh(false)
 end
 
 return DrawerUI
